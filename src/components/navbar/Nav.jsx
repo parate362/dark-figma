09b375c774d92f914f454dd37b17a5b9cb7b5ce2
parate362 <144500446+parate362@users.noreply.github.com>
2024-04-24 14:13:55 +0530

    Update Nav.jsx

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -23,22 +23,9 @@ const NavBar = () => {
             {'  '}
             <img className='d-none d-sm-inline' src="framer.png" alt="" />
           </Navbar.Brand>
-
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(!expanded)} />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ms-auto  pe-3 ">
-              <Nav.Link href="#" className="text-light">About</Nav.Link>
-              <Nav.Link href="#" className="text-light">Features</Nav.Link>
-              <Nav.Link href="#" className="text-light">Customers</Nav.Link>
-              <Nav.Link href="#" className="text-light">Updates</Nav.Link>
-              <Nav.Link href="#" className="text-light">Help</Nav.Link>
-
-              { }
-            </Nav>
-            <Nav>
+          <Nav className='ms-auto'> 
               <button type="button" className="btn btn-light w-115 h-39 fs-6 me-3">Get for free</button>
             </Nav>
-          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
